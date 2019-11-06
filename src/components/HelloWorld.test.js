a2fa679368fe0181a0ddc61722fff5bfc492af9d
村山貴裕 <murayama@sinap.jp>
2019-11-06 11:45:20 +0900

    テストコードをテスト

--- src/components/HelloWorld.test.js
+++ src/components/HelloWorld.test.js
@@ -2,12 +2,11 @@ import { shallowMount } from '@vue/test-utils'
 import HelloWorld from './HelloWorld';
 
 describe('HelloWorld.vue', () => {
-  it('テストケース1', () => {
-    const msg = 'new message'
+  it('メッセージが入るか', () => {
+    const msg = 'new message';
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
-    })
-    // HelloWorldコンポーネント内のテキストを正規表現でチェック(.toMatch(regexp))
-    expect(wrapper.text()).toMatch(msg)
+    });
+    expect(wrapper.text()).toMatch(msg);
   })
 })
